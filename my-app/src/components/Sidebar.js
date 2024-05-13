fbb1ca4c0751bcac4015a1b2b50d9b74723590d1
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-13 14:52:27 +0000

    create Sidebar component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar(props) {
+	return <div>
+        <a>My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Paintings</a>
+    </div>;
+}
+
+export default Sidebar;
