69097cd8224c6e3952dbf5c066b7949ea18be43e
Kay-ddggxh <kathrin.welfare@gmail.com>
2024-05-13 14:59:46 +0000

    style sidebar links with sidebar css module

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,14 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
 function Sidebar(props) {
-	return <div>
-        <a>My Photos</a>
-        <a>My Illustrations</a>
-        <a>My Paintings</a>
-    </div>;
+	return (
+		<div className={css.sidebar}>
+			<a href="">My Photos</a>
+			<a href="">My Illustrations</a>
+			<a href="">My Paintings</a>
+		</div>
+	);
 }
 
 export default Sidebar;
